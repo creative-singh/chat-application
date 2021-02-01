e60a8ed54fe3334fcc34035053af18ec610d8141
Bhavleen <singhbhavleen3@gmail.com>
2021-02-02 00:29:44 +0530

    Chat Application -1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+const App = () => {
+  return (
+    <div>
+      <ChatEngine
+        height="100vh"
+        projectID="c4dc86e6-0501-45d0-8ac2-3fabe56cc83a"
+        userName="bhavleen"
+        userSecret="bhavleen"
+      />
+    </div>
+  );
+};
+
+export default App;
